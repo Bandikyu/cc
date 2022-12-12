eaf83117c87460d0845e78981ab7945d2a4565de
Bandikyu <bripoll.19@gmail.com>
2022-12-11 21:28:30 -0300

    9-crud

---
+++ 9-crud/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+require('dotenv').config();
+
+
+app.use(express.urlencoded());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./src/views"));
+
+const connection = require("./src/models/connection");
+
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM productos", (error, results, field) => {
+    // console.log(error, results, field);
+    if(error) {
+      throw(error);
+    }
+  })
+  res.render("index");
+});
+
+app.use("/posts", require("./src/routes/posts"));
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
